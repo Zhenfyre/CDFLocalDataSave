c2db361814bf3310afa473a17ae20a320de3da36
Jennifer Carrera <jennifer.s.carrera@gmail.com>
2022-04-12 17:16:45 -0400

    Add Basic Location Button

--- app/src/main/java/cdfflint/pilot/cdflocaldatasave/AddEditRecordActivity.java
+++ app/src/main/java/cdfflint/pilot/cdflocaldatasave/AddEditRecordActivity.java
@@ -2,17 +2,26 @@ package cdfflint.pilot.cdflocaldatasave;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -23,8 +32,15 @@ import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class AddEditRecordActivity extends AppCompatActivity {
@@ -102,10 +118,35 @@ public class AddEditRecordActivity extends AppCompatActivity {
     private EditText editTextNitriteResult;
     private EditText editTextNitrateResult;
 
+    Button locationButton;
+    TextView textView1, textView2, textView3, textView4, textView5;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_record);
+
+        locationButton = findViewById(R.id.location_button);
+        textView1 = findViewById(R.id.text_view1);
+        textView2 = findViewById(R.id.text_view2);
+        textView3 = findViewById(R.id.text_view3);
+        textView4 = findViewById(R.id.text_view4);
+        textView5 = findViewById(R.id.text_view5);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        locationButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (ActivityCompat.checkSelfPermission(AddEditRecordActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
+                } else {
+                    ActivityCompat.requestPermissions(AddEditRecordActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                }
+            }
+        });
 
         editTextDate = findViewById(R.id.collectionDate);
         editTextTimeCollected = findViewById(R.id.time_of_day);
@@ -168,6 +209,45 @@ public class AddEditRecordActivity extends AppCompatActivity {
         } else {
             setTitle("Add Record");
         }
+    }
+
+    private void getLocation() {
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location != null) {
+                    try {
+                        Geocoder geocoder = new Geocoder(AddEditRecordActivity.this,
+                                Locale.getDefault());
+                        List<Address> addresses = geocoder.getFromLocation(
+                                location.getLatitude(), location.getLongitude(), 1);
+                        textView1.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Latitude :</b><br></font>"
+                                        + addresses.get(0).getLatitude()
+                        ));
+                        textView2.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Longitude :</b><br></font>"
+                                        + addresses.get(0).getLongitude()
+                        ));
+                        textView3.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Locality :</b><br></font>"
+                                        + addresses.get(0).getLocality()
+                        ));
+                        textView4.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Zip Code :</b><br></font>"
+                                        + addresses.get(0).getPostalCode()
+                        ));
+                        textView5.setText(Html.fromHtml(
+                                "<font color='#6200EE'><b>Address :</b><br></font>"
+                                        + addresses.get(0).getAddressLine(0)
+                        ));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
     private void saveRecord() {
